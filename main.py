cb72514dc8ed63beb545879347ad05a868264f4c
akmal <akm.mp24@gmail.com>
2020-10-05 20:01:23 +0700

    upadate main.py
    mrngapus fungsi jawaban

--- main.py
+++ main.py
@@ -11,16 +11,7 @@ if __name__ == '__main__':
     def Clearscreen():
         import os
         os.system("clear")
-
-
-    def jawaban():
-        print("""
-a b c d e f g h i j  k  l  m  n  o  p
-1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
-q  r  s  t  u  f  w  x  y  z
-17 18 19 20 21 22 23 24 25 26
-        """)
-
+        
 
     def lagi_bro():
         lagi = input("\nMau maen lagi? (y/n)")
@@ -44,7 +35,6 @@ q  r  s  t  u  f  w  x  y  z
                 print("\nanda benar")
             else:
                 print("\nanda salah")
-            jawaban()
         else:
             print("\nhuruf abjad cuma ada 26")
 
